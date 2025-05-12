a7f43b2dd8af3ce1f66d5e6c33315fe7df99d03e
milesbailey121 <milesbailey121@gmail.com>
2025-05-12 20:45:15 +0100

    Refactored CLI code allowing for better testing

--- src/cellpose_quantification/cli.py
+++ src/cellpose_quantification/cli.py
@@ -3,19 +3,7 @@ import os
 import sys
 from . import quantification
 
-def parse_args():
-    parser = argparse.ArgumentParser(description="Quantify cell features from images and masks.")
-    parser.add_argument("image_directory", help="Path to the directory containing images.")
-    parser.add_argument("mask_directory", help="Path to the directory containing masks.")
-    parser.add_argument("marker_path", help="Path to the markers CSV file.")
-    parser.add_argument("-n", "--norm",
-                        nargs='?', const="None", 
-                        choices=["log","minmax","None"],
-                        help="Normalisation technique to use. Choose 'minmax' or 'log'. If not provided, no normalisation is applied.")
-
-    return parser.parse_args()
-
-def validate_args(image_directory, mask_directory, marker_path, normalisation):
+def validate_args(parser,image_directory, mask_directory, marker_path, normalisation):
     image_directory = os.path.abspath(image_directory)
     mask_directory = os.path.abspath(mask_directory)
     marker_path = os.path.abspath(marker_path)
@@ -25,6 +13,7 @@ def validate_args(image_directory, mask_directory, marker_path, normalisation):
     if not os.path.exists(mask_directory):
         raise FileNotFoundError(f"Mask directory '{mask_directory}' does not exist.")
     if not os.path.exists(marker_path):
+        parser.error(f"Marker CSV file '{marker_path}' does not exist.")
         raise FileNotFoundError(f"Marker CSV file '{marker_path}' does not exist.")
     
     if normalisation and normalisation not in ["minmax", "log"]:
@@ -32,22 +21,32 @@ def validate_args(image_directory, mask_directory, marker_path, normalisation):
     
     return image_directory, mask_directory, marker_path, normalisation
 
-def cli():
+def cli(argv=None):
     """
     Entry point for CLI.
     """
-    try:
-        # if len(sys.argv) > 5 or len(sys.argv) < 3:
-        #     print("Usage: python RunQuantification-CLI.py <image_directory> <mask_directory> <marker_path> <normalization>")
-        # else:
-        args = parse_args()
-        image_directory, mask_directory, marker_path, normalisation = validate_args(
+    parser = argparse.ArgumentParser(description="Quantify cell features from images and masks.",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("-v", "--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument("image_directory", help="Path to the directory containing images.")
+    parser.add_argument("mask_directory", help="Path to the directory containing masks.")
+    parser.add_argument("marker_path", help="Path to the markers CSV file.")
+    parser.add_argument("-n", "--norm",
+                        nargs='?', const="None", 
+                        choices=["log","minmax","None"],
+                        help="Normalisation technique to use. Choose 'minmax' or 'log'. If not provided, no normalisation is applied.")
+
+
+    args = parser.parse_args(argv)
+
+    # Check if the arguments are provided
+    image_directory, mask_directory, marker_path, normalisation = validate_args(
+                parser,
                 args.image_directory, args.mask_directory, args.marker_path, args.norm
         )
-        quantification.run(image_directory, mask_directory, marker_path, normalisation)
+    
+    quantification.run(image_directory, mask_directory, marker_path, normalisation)
 
-    except Exception as e:
-        print(f"Error: {e}",file=sys.stderr)
+
 
 if __name__ == "__main__":
     cli()
